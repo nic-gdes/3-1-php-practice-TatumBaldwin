ef509e696e5724241569cddb5bbf7fb1890f5b0b
Tatum Baldwin <tjbaldwin@students.nic.edu>
2024-01-22 15:48:00 -0800

    practice

---
+++ contact.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Page</title>
+</head>
+<body>
+    <head>
+    <?php
+        $page_title = "Contact";
+        include('./head.php');
+    ?>
+    </head>
+
+    <?php include('./nav.php'); ?>
+    <h1>Contact Page</h1>
+
+    <?php include('./footer.php'); ?>
+</body>
+</html>
+
+<?php
+?>
+
+<!-- include('./components/nav.php'); -->
---
+++ footer.php
@@ -0,0 +1,3 @@
+<footer>
+    <h2>&copy; 2024 GDES</h2>
+</footer>
